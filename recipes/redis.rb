61b3a70012833f7ff4c221b3acbff56d8403f77b
Kalilou Diaby <kalilou.diaby@blocket.se>
2016-08-08 21:54:38 +0200

    Add some recipe file and template file

--- recipes/redis.rb
+++ recipes/redis.rb
@@ -4,3 +4,27 @@
 #
 # All rights reserved 
 #
+
+package 'epel-release'
+
+package 'nrpe'
+package 'redis'
+package 'nagios-plugins-all'
+
+service 'redis' do 
+	action [ :enable, :start ]
+end
+
+template '/etc/nagios/nrpe.cfg' do 
+		source 'redis_nrpe.cfg.erb'
+		owner 'nagios'
+		group 'nagios'
+		mode 0664
+		action :create
+		variables(
+			nagios_server_ip: node['nagios_server_ip'] 
+		)  
+end
+
+
+
