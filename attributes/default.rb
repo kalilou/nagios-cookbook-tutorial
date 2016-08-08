b76ae8f43f7f5cacb3bc4d4e8153cea768e7ca88
Kalilou Diaby <kalilou.diaby@blocket.se>
2016-08-08 21:56:46 +0200

    Add some changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
 default['contact_info'] = 'your_email_address'
 default['auth_nagios'] = 0
 default['nagios_pid_file'] = '/usr/local/nagios/var/nagios.pid'
+
+default['nagios_server_ip'] = '192.168.50.200'
